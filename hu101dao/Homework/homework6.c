eb08178667f0e1bc63a0e488abc54b9bcceff282
hu101dao <tyunikutyuzei_tanakasan@docomo.ne.jp>
2020-05-30 20:28:40 +0900

    homework6.c再提出

--- hu101dao/Homework/homework6.c
+++ hu101dao/Homework/homework6.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 
-char c[11];
+void output(char[]);
 
 int main(void)
 {
+    char c[11];
     printf("10文字以内、半角英数で名前を入力ｾｲｰ\n");
-
-    scanf("%s", c);
-
-    printf("いつから%c%c%c%c%c%c%c%c%c%cだと錯覚していた？",c[0], c[1], c[2],c[3],c[4],c[5],c[6],c[7],c[8],c[9]);
-
+    scanf("%s",c);
+    output(c);
+    
     return 0;
 }
 
-
+void output(char c[])
+{
+    int i;
+    for(i = 0; c[i] != '\0' ; i++)
+    {
+        printf("%c",c[i]);
+    }
+} 
